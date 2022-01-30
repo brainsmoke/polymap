7c6650f5555ce2744a4307dc04ef73f77437a943
Gonzalo <godiard@gmail.com>
2022-01-29 23:03:36 -0300

    More pycodestyle fixes

--- maps.py
+++ maps.py
@@ -1,9 +1,11 @@
 
 import os
 
+
 def list_maps():
     files = os.listdir(os.path.join(os.path.dirname(__file__), 'maps'))
-    return [ x[:x.rfind('.paths')] for x in files if x.endswith('.paths') ]
+    return [x[:x.rfind('.paths')] for x in files if x.endswith('.paths')]
+
 
 def map_file(name):
     return os.path.join(os.path.dirname(__file__), 'maps', name + '.paths')
